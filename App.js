845d2fce61d4b2dd782146953d2be1c3c456152e
Filip <filipsarovic428@gmail.com>
2022-11-07 18:55:40 +0100

    Finished with Quiz section

--- App.js
+++ App.js
@@ -5,8 +5,12 @@ import { StackNavigator } from 'react-navigation';
 
 import { Video } from './app/views/Video';
 import { VideoDetail } from './app/views/VideoDetail';
+
 import { Register } from './app/views/Register';
 import { Login } from './app/views/Login';
+
+import { Quiz } from './app/views/Quiz';
+import { Finish } from './app/views/QuizFinish';
 
 const MyRoutes = StackNavigator({
   HomeRT: {
@@ -26,6 +30,12 @@ const MyRoutes = StackNavigator({
   },
   LoginRT: {
     screen: Login
+  },
+  QuizRT: {
+    screen: Quiz
+  },
+  FinishRT: {
+    screen: Finish
   }
 },
   {
